eb69faf3cf27c4c282ec62a90ba21470bf5a9624
Jonathan N.A.W <jonathan22001@mail.unpad.ac.id>
2024-06-08 22:45:26 +0700

    color picker

--- color_extraction.py
+++ color_extraction.py
@@ -1,13 +1,35 @@
 import numpy as np
 import cv2
+from collections import Counter
 
-def detect_color(image):
+def rgb_to_name(rgb_tuple):
+    # Convert RGB to HEX
+    hex_color = "#{:02x}{:02x}{:02x}".format(rgb_tuple[0], rgb_tuple[1], rgb_tuple[2])
+    
+    # Define some common color names
+    color_names = {
+        "#000000": "Black", "#FFFFFF": "White", "#FF0000": "Red", "#00FF00": "Green", "#0000FF": "Blue",
+        "#FFFF00": "Yellow", "#FFA500": "Orange", "#800080": "Purple", "#00FFFF": "Cyan", "#A52A2A": "Brown",
+        "#808080": "Gray", "#FFC0CB": "Pink"
+    }
+    
+    # Return the color name if found, otherwise return the HEX value
+    return color_names.get(hex_color, hex_color)
+
+def detect_colors(image, num_colors=5):
+    # Convert image to RGB format
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
+    # Reshape the image to be a list of pixels
     pixels = image.reshape((-1, 3))
 
-    unique, counts = np.unique(pixels, axis=0, return_counts=True)
+    # Count the unique colors and their frequencies
+    pixel_counts = Counter(map(tuple, pixels))
 
-    most_frequent_color = unique[np.argmax(counts)]
+    # Get the most common colors
+    common_colors = pixel_counts.most_common(num_colors)
 
-    return most_frequent_color
+    # Convert to a list of (RGB, Name) tuples
+    result = [(color, rgb_to_name(color)) for color, count in common_colors]
+
+    return result
--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import streamlit as st
 import numpy as np
 from PIL import Image
 import cv2
-from color_extraction import detect_color
+from color_extraction import detect_colors
 
 def main():
     st.title("Color Detector")
-    st.write("Upload an image to detect the most dominant color.")
+    st.write("Upload an image to detect the five most dominant colors.")
 
     uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 
@@ -16,11 +16,15 @@ def main():
         st.write("")
         st.write("Detecting colors...")
 
+        # Convert the image to an OpenCV format
         image = np.array(image)
         
-        most_frequent_color = detect_color(image)
+        dominant_colors = detect_colors(image, num_colors=5)
 
-        st.write(f"The most dominant color is: RGB {most_frequent_color}")
+        st.write("The most dominant colors are:")
+        for i, (color, name) in enumerate(dominant_colors):
+            st.write(f"{i+1}. RGB {color} - {name}")
+            st.markdown(f'<div style="width:50px;height:50px;background-color:rgb({color[0]},{color[1]},{color[2]});"></div>', unsafe_allow_html=True)
 
 if __name__ == "__main__":
     main()
